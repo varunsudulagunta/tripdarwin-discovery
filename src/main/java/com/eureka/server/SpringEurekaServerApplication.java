335abebe4bd9b01f57e2bca6709fd6546b9a6a63
Monika Joshi <monika@techsimians.com>
2023-12-21 12:15:12 +0530

    second commit

--- src/main/java/com/eureka/server/SpringEurekaServerApplication.java
+++ src/main/java/com/eureka/server/SpringEurekaServerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
 @SpringBootApplication
 @EnableEurekaServer		// Enable eureka server
-
 public class SpringEurekaServerApplication {
 
 	public static void main(String[] args) {
